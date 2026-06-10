b4f547331217559910fa385040da6b3edd214423
Berk Arat <berkarat93@hotmail.com>
2026-06-10 17:44:45 +0300

    fix: use IsDevelopment() for API URL instead of config file

--- src/Portlite.Web/Program.cs
+++ src/Portlite.Web/Program.cs
@@ -8,7 +8,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
+var apiBaseUrl = builder.HostEnvironment.IsDevelopment()
+    ? "http://localhost:5101"
+    : builder.HostEnvironment.BaseAddress;
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 builder.Services.AddScoped<ApiClient>();
